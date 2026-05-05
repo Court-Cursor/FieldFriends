9b289946a6e49ca161181ef559ca3051f5ea17b4
Richie Wang <wrongqir@icloud.com>
2026-05-05 11:32:49 +0800

    changed logout button colour

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -19,7 +19,7 @@ export function NavBar() {
         {user ? (
           <>
             <span>{user.email}</span>
-            <button type="button" onClick={logout}>
+            <button className="logout" type="button" onClick={logout}>
               Logout
             </button>
           </>
